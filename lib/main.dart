e2aec1cb58d0e6a92a5475a285d387ef90e62b24
nico-develop <contact@nico-develop.fr>
2022-11-23 07:04:10 +0100

    Ajout de l'internationalisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import "package:flutter/material.dart";
 import "package:shop/route/route_constants.dart";
 import "package:shop/route/router.dart" as router;
+import "package:flutter_localizations/flutter_localizations.dart";
+import "package:flutter_gen/gen_l10n/app_localizations.dart";
 
 import "theme/dark_theme.dart";
 import "theme/light_theme.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +22,16 @@ class MyApp extends StatelessWidget {
       theme: lightTheme(context),
       darkTheme: darkTheme(context),
       themeMode: ThemeMode.light,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("en", ""),
+        Locale("fr", ""),
+      ],
       onGenerateRoute: router.generateRoute,
       initialRoute: signUpScreenRoute,
     );
